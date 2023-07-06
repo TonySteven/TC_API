fcc1ef0181120f581ced665271edca20394a6c3d
Steven Liu <1012987522@qq.com>
2023-07-06 20:01:51 +0800

    :sparkles: feat: 更新代码.

--- demo/decrypt.py
+++ demo/decrypt.py
@@ -1,13 +1,13 @@
 from util.EncryptData import EncryptData
 
 # 定义json路径
-path = r"../json/response.txt"
+path = r"../json/response.json"
 file = open(path, 'r', encoding='utf-8')
 
 file_result = open(r"../json/data.json", 'a')
 
 # 秘钥
-password = 'DJTDLRH3R7M9BINRL5O2E6O7MJM8LUXB'
+password = 'MQ0WINT60DXP7U7R09A70V6Z1SEDIGYH'
 
 # 需要加密的内容, file.read() 读取文件内容
 data = file.read()
--- demo/demo.py
+++ demo/demo.py
@@ -6,8 +6,9 @@ from util.EncryptData import EncryptData
 path = r"../../TC_API/json/request.json"
 file = open(path, 'r', encoding='utf-8')
 
-password = 'DJTDLRH3R7M9BINRL5O2E6O7MJM8LUXB'  # 秘钥
+password = 'MQ0WINT60DXP7U7R09A70V6Z1SEDIGYH'  # 秘钥
 data = json.load(file)  # 需要加密的内容
+
 eg = EncryptData(password)  # 这里**的长度必须是16的倍数
 res = eg.encrypt(str(data))
 print("密文：" + res)
